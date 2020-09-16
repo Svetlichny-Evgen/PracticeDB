837632508fadf75a17140eb9da196d8ac5e6a128
Svetlichny-Evgen <52456350+Svetlichny-Evgen@users.noreply.github.com>
2020-09-16 14:13:15 +0300

    Добавьте файлы проекта.

---
+++ PracticeDB/Auto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDB
+{
+    class Auto
+    {
+        int id_car;
+        string brend;
+        string model;
+        string manufacturer;
+        int cost_of_the_model;
+        float engine_capacity;
+        int engine_power;
+        float the_amount_of_gasoline_per_100_km;
+        string engine_type;
+    }
+}
